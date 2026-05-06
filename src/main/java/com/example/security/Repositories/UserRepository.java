1a499758a7d9aa5a4e1e5a3362283fb88ec820d6
Alberto-Neo <neoalberto15@gmail.com>
2026-05-05 21:17:07 -0300

    fix

--- src/main/java/com/example/security/Repositories/UserRepository.java
+++ src/main/java/com/example/security/Repositories/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
-    UserDetails.findByLogin(String role);
+    UserDetails findByLogin(String role);
 }
